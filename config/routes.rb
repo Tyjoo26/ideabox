13c467a9bb0212c1463600d9b1f66162949d83e6
Tyjoo26 <Tyjoo21@gmail.com>
2017-10-25 20:41:22 -0600

    Reorganize Rake Routes, associations are correct with nested collections and namespaced collections

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,14 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
-  resources :categories, only: [:show, :index]
-
-  resources :ideas,  only:[:show, :index]
-
-  resources :images, only: [:show, :index]
-
   resources :users, only: [:new, :create, :show] do
-    resources :ideas, only: [:new, :create, :update, :destroy]
+    resources :images, only: [:show, :index]
+    resources :categories, only: [:show, :index]
+    resources :ideas, only: [:index, :show, :new, :create, :update, :destroy]
   end
-
 
   namespace :admin do
     resources :categories, only: [:index, :new,:create, :destroy]
-  end
-
-  namespace :admin do
     resources :images, only: [:new, :create]
   end
 
